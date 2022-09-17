80ae9b29bd7d8cf891817e80728a22ab2086afa5
dmitriy <master71512@mail.ru>
2022-09-17 14:33:54 +0300

    n4 solved

---
+++ n4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max)
+{
+    max=b;
+}
+if (c>max)
+{
+    max=c;
+}
+Console.Write("max = " + max);
