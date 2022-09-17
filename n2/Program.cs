b338a01ddac9fc14100715937faeccb478a6fc95
dmitriy <master71512@mail.ru>
2022-09-17 14:28:14 +0300

    n2 solved

---
+++ n2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите 2 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write("max = " + a);
+}
+else
+{
+    if (a<b)
+    {
+      Console.Write("max = " + b);  
+    }
+    else
+    {
+        Console.Write("max = a = b");
+    }
+}
