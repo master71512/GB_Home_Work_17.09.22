d2629063f560ebd4126957c760241c3faa4304d0
dmitriy <master71512@mail.ru>
2022-09-17 14:51:29 +0300

    solved n8

---
+++ n8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите положительное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a <= 0)
+{
+    Console.Write("число неположительное!");
+}
+else
+{
+    int count = 2;
+    while (count <= a)
+    {
+        Console.Write(count);
+        count += 2;
+        if (count <= a)
+        {
+            Console.Write(", ");
+        }
+    }
+}
